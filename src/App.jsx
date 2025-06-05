dd26b12a6f5e714f6e08b3589e0f49e964e1f5a7
Your Name <your-email@example.com>
2025-06-05 10:49:33 +0530

    all files of my project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './compoents/Weather'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+      
+    </div>
+  )
+}
+
+export default App
